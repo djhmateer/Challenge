e50c046e13a631f10d10e6e465e4c3bc979ba4aa
Dave Mateer <davemateer@gmail.com>
2019-07-14 09:17:28 +0100

    auto commit

--- Challenge/Program.cs
+++ Challenge/Program.cs
@@ -6,6 +6,14 @@ namespace Challenge
 {
     public class Program
     {
+        // Run the Console App to see Ship1,2,3 runs
+        // Unit tests are in Xunit
+
+        // This is an initial effort to get it working 
+        // and is developed in a TDD style
+
+        // Instructions:
+
         // determines each sequence of ship positions and reports the final position of the ship
         // ship position:
         //   x,y
@@ -28,30 +36,26 @@ namespace Challenge
         // each ship is processed sequentially ie finishes executing before the next ship begins
         // max value of coordinate is 50, all instruction strings < 100 characters in length
 
-        static void Main(string[] args)
+        static void Main()
         {
             while (true)
             {
                 Console.WriteLine("Welcome to the Ship Challenge (x to exit)\n");
-                var ship1 = "5 3\n1 1 E\nRFRFRFRF";
-                var ship2 = "5 3\n3 2 N\nFRRFLLFFRRFLL";
-                var ship3 = "0 3 W\nLLFFFLFLFL";
-                Console.WriteLine("\n1 for ship1:");
-                Console.WriteLine(ship1);
-                Console.WriteLine("\n2 for ship2:");
-                Console.WriteLine(ship2);
-                Console.WriteLine("\n3 for ship2 and ship3:");
-                Console.WriteLine(ship3);
+                const string ship1 = "5 3\n1 1 E\nRFRFRFRF";
+                const string ship2 = "5 3\n3 2 N\nFRRFLLFFRRFLL";
+                const string ship3 = "0 3 W\nLLFFFLFLFL";
+                Console.WriteLine($"\n1 for ship1:\n{ship1}");
+                Console.WriteLine($"\n2 for ship2:\n{ship2}");
+                Console.WriteLine($"\n3 for ship2 and ship3:\n{ship2}\n{ship3}");
 
                 var keypress = Console.ReadKey().Key;
                 var list = new List<string>();
                 if (keypress == ConsoleKey.X) break;
                 if (keypress == ConsoleKey.D1) list.Add(ship1);
                 if (keypress == ConsoleKey.D2) list.Add(ship2);
-                if (keypress == ConsoleKey.D3) list.AddRange(new[] {ship2, ship3});
-                var output = Run(list.ToArray());
+                if (keypress == ConsoleKey.D3) list.AddRange(new[] { ship2, ship3 });
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"\n\n Output: {output} \n\n");
+                Console.WriteLine($"\n\n Output: {Run(list.ToArray())} \n\n");
                 Console.ResetColor();
             }
         }
@@ -63,30 +67,39 @@ namespace Challenge
         // eg 5 3\n1 1 E\nRFRFRFRF
         static string Run(string[] inputs)
         {
+            // if there are multiple ships the grid is defined only by the first ship
+            // currentPosX, Y, currentOrientation, isLost are defined here as need to be returned from this method
             int gridMaxX = 0, gridMaxY = 0, currentPosX = 0, currentPosY = 0;
             string currentOrientation = null;
             var isLost = false;
+            // warningCoordsList needs to be passed between ships eg see ship2 and ship3 test
             var warningCoordsList = new List<(int x, int y, string direction)>();
 
             // loop over each ship 
             for (var i = 0; i < inputs.Length; i++)
             {
-                // split input into a string array based on newline character
-                var lines = inputs[i].Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
-
-                var startPositionLine = 0;
-                var instructionPositionLine = 1;
+                // split input newline
+                var lines = inputs[i].Split("\n");
 
                 // if multiple ships then only the first includes the grid size on the first line
+                var gridSizeLine = 0;
+                var startPositionLine = 1;
+                var instructionPositionLine = 2;
+
+                // if it is the first ship assume gridsize is on first line
                 if (i == 0)
                 {
                     // make the grid based on the the first line
-                    var gridSizeStringArray = lines[0].Split(' '); // eg 5 3  assuming a single space
+                    var gridSizeStringArray = lines[gridSizeLine].Split(' '); // eg 5 3  assuming a single space
                     gridMaxX = int.Parse(gridSizeStringArray[0]); // eg 5 
                     gridMaxY = int.Parse(gridSizeStringArray[1]); // eg 3
                     if (gridMaxX > 50 || gridMaxY > 50) throw new ArgumentException("Max grid size is 50");
-                    startPositionLine++;
-                    instructionPositionLine++;
+                }
+                else
+                // not the first ship so first line will be startPositionLine.. 
+                {
+                    startPositionLine--;
+                    instructionPositionLine--;
                 }
 
                 // get starting position of the ship
@@ -102,13 +115,12 @@ namespace Challenge
                 if (instructionsString.Length >= 100)
                     throw new ArgumentException("Instructions should be less than 100");
 
-                // 0,0 is lower left, and 5,3 is top right
-
-                // iterate over each instruction
+                // set current position and orientation of ship (starting position)
                 currentPosX = startPosX;
                 currentPosY = startPosY;
                 currentOrientation = startPosOrientation;
                 isLost = false;
+                // iterate over each instruction
                 foreach (char c in instructionsString)
                 {
                     var instruction = c.ToString();
@@ -122,13 +134,12 @@ namespace Challenge
 
                     if (isLost)
                     {
-                        // add to the warningCoordsList
+                        // add to the warningCoordsList for next ship if there is one
                         warningCoordsList.Add((currentPosX, currentPosY, currentOrientation));
-                        break; // Gone off the grid and lost
+                        break; // out of foreach. Gone off the grid and lost
                     }
                 }
             }
-
             return currentPosX + " " + currentPosY + " " + currentOrientation + (isLost ? " LOST" : null);
         }
 
